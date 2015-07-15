325047d653bbe13bb8e9418aa242943d3534ba6d
NSemakov <nsemakov@yahoo.com>
2015-07-16 01:58:49 +0400

    Initial Commit

---
+++ 9. Delegates/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  9. Delegates
+//
+//  Created by Admin on 16.07.15.
+//  Copyright (c) 2015 Admin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 9. Delegates/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  9. Delegates
+//
+//  Created by Admin on 16.07.15.
+//  Copyright (c) 2015 Admin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
